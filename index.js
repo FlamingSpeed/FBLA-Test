cc1139dae28ccd74ff6f657f9e7e768620d013ad
Kevin Siju Eappen <126803492+FlamingSpeed@users.noreply.github.com>
2024-08-29 12:21:36 -0600

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PersonalFinanceAnalyzer from './PersonalFinanceAnalyzer';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<PersonalFinanceAnalyzer />);
